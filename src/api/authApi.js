86db16a3d1d41d9035d566470b9a1bdd06cdec00
SujalPrajapati2006 <prajapatisujal123456@gmail.com>
2026-05-26 22:27:37 +0530

    Fix LOGIN issue

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -28,7 +28,11 @@ api.interceptors.response.use(
   async (error) => {
     const original = error.config;
 
-    if (error.response?.status === 401 && !original._retry) {
+    if (
+      error.response?.status === 401 &&
+      !original._retry &&
+      !original.url.includes("/auth/login")
+    ) {
       if (isRefreshing) {
         return new Promise((resolve, reject) => {
           failedQueue.push({ resolve, reject });
